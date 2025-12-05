18aec82afc572a1a94edcd0a6ab749bf2eae47ca
zhangxintong <zhangxintong@bigai.ai>
2025-12-05 14:16:47 +0800

    docs: Update README

--- slurm_jobs/eval/inference_vstar.sh
+++ slurm_jobs/eval/inference_vstar.sh
@@ -14,3 +14,24 @@ python -u ./eval/vstar/judge_result.py \
     
 wait 
 echo "Finish inference_vstar"
+
+
+
+LOG_FILE=/your/path/to/log/file
+mkdir -p $LOG_FILE
+
+
+python -u ./eval/vstar/vllm_inference.py \
+    --scaleup_factor 2 \
+    --min_resolution 112 \
+    --enlarge_factor 1.5 \
+    --vstar_bench_path /your/path/to/vstar_bench \
+    --model_path /your/path/to/model \
+    --save_name inference_vstar \
+    --save_dir /your/dir/to/save/result \
+    2>&1 | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush(); }' > $LOG_FILE/inference_vstar.log &
+
+wait
+echo "Finish inference"
+
+
--- slurm_jobs/eval/metrics_vstar.sh
+++ slurm_jobs/eval/metrics_vstar.sh
@@ -2,17 +2,16 @@ LOG_FILE=/your/path/to/log/file
 mkdir -p $LOG_FILE
 
 
-python -u ./eval/vstar/vllm_inference.py \
-    --scaleup_factor 2 \
-    --min_resolution 112 \
-    --enlarge_factor 1.5 \
+API_URL=
+
+python -u ./eval/vstar/judge_result.py \
+    --api_url $API_URL \
     --vstar_bench_path /your/path/to/vstar_bench \
     --model_path /your/path/to/model \
     --save_name inference_vstar \
     --save_dir /your/dir/to/save/result \
-    2>&1 | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush(); }' > $LOG_FILE/inference_vstar.log &
-
-wait
-echo "Finish inference"
-
+    2>&1 | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush(); }' > $LOG_FILE/metrics_vstar.log &
+    
+wait 
+echo "Finish inference_vstar"
 
